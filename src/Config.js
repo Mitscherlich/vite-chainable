89011c9ba727d0230767961bc6d0fb90965b06f3
Mitscherlich Wei <mitscherlich36@gmail.com>
2022-07-03 23:39:01 +0800

    feat: add `Config.toConfig` & `Config.toString`

--- src/Config.js
+++ src/Config.js
@@ -1,3 +1,4 @@
+import { stringify } from 'javascript-stringify'
 import { Build } from './Build'
 import { ChainedMap } from './ChainedMap'
 // import { ChainedSet } from './ChainedSet'
@@ -42,8 +43,67 @@ export class Config extends ChainedMap {
     ])
   }
 
+  static toString(config, { verbose = false, configPrefix = 'config' } = {}) {
+    return stringify(config, (value, indent, stringify) => {
+      // improve plugin output
+      if (value && value.__pluginName) {
+        const prefix = `/* ${configPrefix}.${value.__pluginType}('${value.__pluginName}') */\n`
+        const constructorExpression = value.__pluginPath
+          // The path is stringified to ensure special characters are escaped
+          // (such as the backslashes in Windows-style paths).
+          ? `(require(${stringify(value.__pluginPath)}))`
+          : value.__pluginConstructorName
+
+        if (constructorExpression) {
+          // get correct indentation for args by stringifying the args array and
+          // discarding the square brackets.
+          const args = stringify(value.__pluginArgs).slice(1, -1)
+          return `${prefix}new ${constructorExpression}(${args})`
+        }
+
+        return prefix + stringify(value.__pluginArgs && value.__pluginArgs.length
+          ? { args: value.__pluginArgs }
+          : {})
+      }
+
+      if (value && value.__expression)
+        return value.__expression
+
+      // shorten long functions
+      if (typeof value === 'function') {
+        if (!verbose && value.toString().length > 100)
+          return 'function () { /* omitted long function */ }'
+      }
+
+      return stringify(value)
+    }, 2)
+  }
+
   plugin(name) {
     return this.plugins.getOrCompute(name, () => new Plugin(this, name))
+  }
+
+  toConfig() {
+    return this.clean(
+      Object.assign(this.entries() || {}, {
+        define: this.define.entries(),
+        plugins: this.plugins.values().map(plugin => plugin.toConfig()),
+        resolve: this.resolve.toConfig(),
+        css: this.css.toConfig(),
+        json: this.json.toConfig(),
+        // esbuild: this.esbuild.toConfig(),
+        server: this.server.toConfig(),
+        build: this.build.toConfig(),
+        preview: this.preview.toConfig(),
+        optimizeDeps: this.optimizeDeps.toConfig(),
+        ssr: this.ssr.toConfig(),
+        worker: this.worker.toConfig(),
+      }),
+    )
+  }
+
+  toString(options) {
+    return Config.toString(this.toConfig(), options)
   }
 
   merge(obj = {}, omit = []) {
